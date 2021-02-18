e2c9fbb1b1a23563bd3f724094b49c3ccc47523a
Jakub Čajka <jcajka@redhat.com>
2021-02-18 11:09:21 +0100

    Reabse to go1.16 Even better bundled provides

--- bundled-deps.sh
+++ bundled-deps.sh
@@ -17,6 +17,7 @@
 
 provides=""
 for bundle in $(find -name modules.txt); do
-provides="$provides\n$(cat "$bundle" | grep "^# " | grep -v "# explicit" | sed -r s/"^#.* => "// | sed -r "s/# //" | sed -r "s:(.*) (.*):Provides\: bundled(golang(\1)) = \2:")"
+provides="$provides\n$(cat "$bundle" | grep "^# " | grep -v "# explicit" | sed -r s/"^#.* => "// | sed -r "s/# //" | sed -r "s:(.*) v(.*):Provides\: bundled(golang(\1)) = \2:")"
 done
+#TODO replace - with . in version per packaging guidelines
 echo -e "$provides" | sort -u
